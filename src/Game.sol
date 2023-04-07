819e909181e64a76460fb448d84b0088ed9fca5f
Yacine Badiss <yacine.badiss@ledger.fr>
2023-04-07 10:41:58 +0100

    Follow eip-173 for ownership

--- src/Game.sol
+++ src/Game.sol
@@ -1,9 +1,17 @@
 //SPDX-License-Identifier: Unlicense
 pragma solidity >=0.8.13;
 
-contract Ownable {
+interface IOwnableEvents {
+    /// @dev This emits when ownership of a contract changes.
+    event OwnershipTransferred(address indexed previousOwner, address indexed newOwner);
+}
+
+contract Ownable is IOwnableEvents {
+    // TODO how do I properly define that we support interface 0x7f5828d0?
+
     address public owner;
 
+    /// Errors
     error NotOwner();
 
     /// @notice Modifier to enforce ownership control
@@ -12,10 +20,13 @@ contract Ownable {
         _;
     }
 
-    /// @notice Set owner of contract, can only be done by the current owner
-    /// @param newOwner New owner of the contract
-    function setOwner(address newOwner) public onlyOwner {
-        owner = newOwner;
+    /// @notice Set the address of the new owner of the contract
+    /// @dev Set _newOwner to address(0) to renounce any ownership.
+    /// @param _newOwner The address of the new owner of the contract
+    function transferOwnership(address _newOwner) external onlyOwner {
+        address previousOwner = owner;
+        owner = _newOwner;
+        emit OwnershipTransferred(previousOwner, owner);
     }
 }
 
@@ -46,6 +57,7 @@ contract Game is Ownable {
     /// @param _owner New owner of the contract
     constructor(address _owner) {
         owner = _owner;
+        emit OwnershipTransferred(address(0), owner);
     }
 
     //////////////////////
--- test/Game.t.sol
+++ test/Game.t.sol
@@ -4,36 +4,39 @@ pragma solidity ^0.8.19;
 import "forge-std/Test.sol";
 import "../src/Game.sol";
 
-contract GameTest is Test {
+contract OwnableTest is Test, IOwnableEvents {
     Game public game;
     address public owner = address(1);
 
     function setUp() public {
+        vm.expectEmit();
+        emit OwnershipTransferred(address(0), owner);
+
         game = new Game(owner);
     }
 
-    function test_ownerIsSet() public {
-        assertEq(game.owner(), owner);
-    }
+    function test_transferOwnership() public {
+        address newOwner = address(2);
 
-    function test_setOwner() public {
-        address newOwner = address(0);
-
+        vm.expectEmit();
+        emit OwnershipTransferred(owner, newOwner);
         vm.prank(owner);
-        game.setOwner(newOwner);
+        game.transferOwnership(newOwner);
         assertEq(game.owner(), newOwner);
 
+        vm.expectEmit();
+        emit OwnershipTransferred(newOwner, owner);
         vm.prank(newOwner);
-        game.setOwner(owner);
+        game.transferOwnership(owner);
         assertEq(game.owner(), owner);
     }
 
-    function test_setOwner_RevertIf_notOwner() public {
-        address notOwner = address(0);
+    function test_transferOwnership_RevertIf_notOwner() public {
+        address notOwner = address(2);
         vm.startPrank(notOwner);
 
         vm.expectRevert(Ownable.NotOwner.selector);
-        game.setOwner(notOwner);
+        game.transferOwnership(notOwner);
     }
 }
 
@@ -57,7 +60,7 @@ contract BossTest is Test {
     }
 
     function test_setBoss_RevertIf_notOwner() public {
-        address notOwner = address(0);
+        address notOwner = address(2);
         vm.startPrank(notOwner);
 
         vm.expectRevert(Ownable.NotOwner.selector);
