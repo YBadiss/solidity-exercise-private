99b5e530b6fb05f7a5035d96f09baa1a046451b1
Yacine Badiss <yacine.badiss@ledger.fr>
2023-04-07 09:27:16 +0100

    Add basic ownership of contract

--- src/Game.sol
+++ src/Game.sol
@@ -1,11 +1,34 @@
 //SPDX-License-Identifier: Unlicense
 pragma solidity >=0.8.13;
 
-// TODO: Implement all user stories and one of the feature request
-contract Game {
-    uint256 public x;
+contract Ownable {
+    address public owner;
 
-    constructor() {
-        x = 4;
+    error NotOwner();
+
+    /// @notice Modifier to enforce ownership control
+    modifier onlyOwner {
+        if (msg.sender != owner) revert NotOwner();
+        _;
+    }
+
+    /// @notice Set owner of contract, can only be done by the current owner
+    /// @param newOwner New owner of the contract
+    function setOwner(address newOwner) public onlyOwner {
+        owner = newOwner;
+    }
+}
+
+/// @title World Of Ledger
+/// @author Yacine B. Badiss
+/// @notice Create a character linked to your address and fight monsters!
+/// @dev Main contract controlling the game flow
+contract Game is Ownable {
+
+    /// @notice Instantiate a new contract and set its owner
+    /// @dev `owner` is defined in the Ownable interface
+    /// @param _owner New owner of the contract
+    constructor(address _owner) {
+        owner = _owner;
     }
 }
--- test/Game.t.sol
+++ test/Game.t.sol
@@ -6,12 +6,35 @@ import "../src/Game.sol";
 
 contract GameTest is Test {
     Game public game;
+    address public owner;
 
     function setUp() public {
-        game = new Game();
+        owner = address(1);
+        game = new Game(owner);
     }
 
-    function testCompany() public {
-        assertEq(game.x(), 4);
+    function test_ownerIsSet() public {
+        assertEq(game.owner(), owner);
+    }
+
+    function test_setOwner_RevertIf_notOwner() public {
+        vm.expectRevert(Ownable.NotOwner.selector);
+
+        address notOwner = address(0);
+
+        vm.prank(notOwner);
+        game.setOwner(notOwner);
+    }
+
+    function test_setOwner() public {
+        address newOwner = address(0);
+
+        vm.prank(owner);
+        game.setOwner(newOwner);
+        assertEq(game.owner(), newOwner);
+
+        vm.prank(newOwner);
+        game.setOwner(owner);
+        assertEq(game.owner(), owner);
     }
 }
