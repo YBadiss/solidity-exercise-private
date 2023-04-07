7283d50225cb47d843c715f8e8faa6c697f20801
Yacine Badiss <yacine.badiss@ledger.fr>
2023-04-07 09:52:08 +0100

    Add basic Boss

--- src/Game.sol
+++ src/Game.sol
@@ -1,6 +1,40 @@
 //SPDX-License-Identifier: Unlicense
 pragma solidity >=0.8.13;
 
+contract Boss {
+    string public name;
+    uint256 public hp;
+    uint256 public damage;
+    uint256 public xpReward;
+
+    /// @notice Instantiate a new Boss object
+    /// @dev The parameters should be generated randomly
+    /// @param _name Name of the Boss
+    /// @param _hp Life points of the Boss
+    /// @param _damage Damage inflicted by the Boss on each attack
+    /// @param _xpReward Experience reward split between all fighters
+    constructor(string memory _name, uint256 _hp, uint256 _damage, uint256 _xpReward) {
+        name = _name;
+        hp = _hp;
+        damage = _damage;
+        xpReward = _xpReward;
+    }
+
+    /// @notice Check if the Boss is dead
+    /// @dev hp is unsigned, we don't check negative values
+    /// @return bool true if dead, false otherwise
+    function isDead() view public returns(bool) {
+        return hp == 0;
+    }
+
+    /// @notice Hit the Boss
+    /// @dev We make sure that hp does not go below 0 since it is unsigned
+    /// @param _damage The amount of hp the Boss must lose
+    function hit(uint256 _damage) public {
+        hp = _damage >= hp ? 0 : hp - _damage;
+    }
+}
+
 contract Ownable {
     address public owner;
 
--- test/Game.t.sol
+++ test/Game.t.sol
@@ -6,10 +6,9 @@ import "../src/Game.sol";
 
 contract GameTest is Test {
     Game public game;
-    address public owner;
+    address public owner = address(1);
 
     function setUp() public {
-        owner = address(1);
         game = new Game(owner);
     }
 
@@ -36,5 +35,41 @@ contract GameTest is Test {
         vm.prank(newOwner);
         game.setOwner(owner);
         assertEq(game.owner(), owner);
+    }
+}
+
+contract BossTest is Test {
+    Boss public boss;
+    string public name = "Test Boss";
+    uint256 public hp = 1000;
+    uint256 public damage = 50;
+    uint256 public xpReward = 10000;
+
+    function setUp() public {
+        boss = new Boss(name, hp, damage, xpReward);
+    }
+
+    function test_bossIsSet() public {
+        assertEq(boss.name(), name);
+        assertEq(boss.hp(), hp);
+        assertEq(boss.damage(), damage);
+        assertEq(boss.xpReward(), xpReward);
+    }
+
+    function test_bossTakesHit() public {
+        assertEq(boss.hp(), hp);
+        boss.hit(10);
+        assertEq(boss.hp(), hp - 10);
+    }
+
+    function test_bossHpAlwaysAboveZero() public {
+        boss.hit(boss.hp() + 1);
+        assertEq(boss.hp(), 0);
+    }
+
+    function test_bossIsDead() public {
+        assertFalse(boss.isDead());
+        boss.hit(boss.hp());
+        assertTrue(boss.isDead());
     }
 }
